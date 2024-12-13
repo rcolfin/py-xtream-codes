a452f5960083c06399628e3b469a656f3f67278b
Robert Colfin <rcolfin@hotmail.com>
2024-12-13 12:21:03 -0500

    adding __version__ to the __init__.py

--- xtream/__init__.py
+++ xtream/__init__.py
@@ -1,3 +1,15 @@
+from __future__ import annotations
+
+import importlib
+
 from xtream._xtream import XTream
 
-__all__ = ["XTream"]
+# set the version number within the package using importlib
+try:
+    __version__: str | None = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = None
+
+
+__all__ = ["XTream", "__version__"]
